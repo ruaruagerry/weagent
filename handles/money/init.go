be7a4398635bbea7b6b523480f0ea49ab23ef623
GerryLuo <dkluohanga@163.com>
2019-12-13 12:02:59 +0800

    挪到web

--- handles/money/init.go
+++ handles/money/init.go
@@ -10,6 +10,5 @@ func init() {
 	server.RegisterPostHandle("/money/ad/record", adRecordHandle)         // 查看广告收益记录
 	server.RegisterGetHandle("/money/entrance", entranceHandle)           // 主界面
 	server.RegisterGetHandle("/money/getout/apply", getoutApplyHandle)    // 提现申请
-	server.RegisterGetHandle("/money/getout/result", getoutResultHandle)  // 提现审核结果通知
 	server.RegisterPostHandle("/money/getout/record", getoutRecordHandle) // 查看提现记录
 }
